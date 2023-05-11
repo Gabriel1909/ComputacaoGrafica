0778513e9f163149d79612e4289173608eefc937
Gabriel Bittencourt Gamboa <GABRIEL28@edu.unisinos.br>
2023-05-10 22:00:20 -0300

    Iniciado HelloTexture adicionado vários objetos

---
+++ HelloTexture/Hello3D - Pyramid/Hello3D.fs
@@ -0,0 +1,33 @@
+#version 400
+
+in vec3 finalColor;
+in vec3 scaledNormal;
+in vec3 fragmentPos;
+
+uniform float ka;
+uniform float kd;
+uniform float ks;
+uniform float q;
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 cameraPos;
+
+out vec4 color;
+
+void main() {
+	vec3 ambiente = lightColor * ka;
+
+	vec3 N = normalize(scaledNormal);
+	vec3 L = normalize(lightPos - fragmentPos);
+	float diff = max(dot(N, L), 0.0);
+	vec3 diffuse = diff * lightColor * kd;
+
+	vec3 R = reflect(-L,N);
+	vec3 V = normalize(cameraPos - fragmentPos);
+	float spec = pow(max(dot(R,V),0.0),q);
+	vec3 specular = spec * ks * lightColor;
+
+	vec3 result = (ambiente + diffuse) * finalColor + specular;
+
+	color = vec4(result, 1.0f);
+}
